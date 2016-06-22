b33e044810c3a30d97435744e1d543fbd76d9739
Krasimir Nikolov <krasimir.nikolov@mtr-design.com>
2016-06-22 20:11:32 +0300

    Set local DB settings

--- helpers/database.php
+++ helpers/database.php
@@ -1,6 +1,17 @@
 <?php
+if (file_exists(PATH_ROOT . '/helpers/local.php')) {
+  require_once 'local.php';
+} else {
+  trait Configs {
+      static $host = '127.0.0.1';
+		  static $database = 'fmi-www-local';
+		  static $user = 'root';
+		  static $password = 'password';
+  }
+}
 
 class Database {
+  use Configs;
 
 	/**
 	 * PDO object
@@ -13,13 +24,9 @@ class Database {
 	}
 
 	public function connect() {
-		$host = '127.0.0.1';
-		$database = 'fmi-www-local';
-		$user = 'root';
-		$password = 'password';
 
 		try {
-		  $this->pdo = new PDO('mysql:host='.$host.';dbname='.$database, $user, $password);
+		  $this->pdo = new PDO('mysql:host='.self::$host.';dbname='.self::$database, self::$user, self::$password);
 		  $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		} catch(PDOException $e) {
 		  echo 'Error: ' . $e->getMessage();
